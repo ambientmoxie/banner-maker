846000e95d3b4606ac36f8c4fdca62e1f871ff8a
ambientmoxie <contact@maximebenoit.work>
2024-10-21 14:56:55 +0200

    updated: vite base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "/projects/banner-maker",
+  base: "/tool/banner-maker",
   server: { host: true },
 });
